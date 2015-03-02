dcd018fdca57209df24167cb92ce051a8d677fce
Rohit Kumar Rai <rohitkrai03@gmail.com>
2015-03-02 15:52:08 +0530

    Code Revisions
    Minor bug fixed by using os.path.normpath in source and target arguments.

--- mdgen/mdgen.py
+++ mdgen/mdgen.py
@@ -6,11 +6,11 @@ import utils
 import os
 
 # get the source directory
-source = sys.argv[1]
+source = os.path.normpath(sys.argv[1])
 # get the target directory
-target = sys.argv[2]
+target = os.path.normpath(sys.argv[2])
 
 utils.convert_md_directory(source, target)
 
-
+print("HTML files for your markdown has been generated in {}".format(target))
 
